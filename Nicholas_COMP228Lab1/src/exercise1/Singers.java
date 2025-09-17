80d5ea9dc4573fe3beda5c166e06620a4ff8ac39
Cooljackup <cooljackup139@gmail.com>
2025-09-17 18:53:17 -0400

    Lab 1 completed
    FInished both Singers.java and SingersDriver.java according to document. Uploaded both files.

--- Nicholas_COMP228Lab1/src/exercise1/Singers.java
+++ Nicholas_COMP228Lab1/src/exercise1/Singers.java
@@ -1,5 +1,57 @@
+// COMP228-406 - Lab 1 - Nicholas Bonneville
+// Lab 1 - Exercise 1
+
+// Write a Java application that creates a Java console application to keep records of singers and displays stored records.
+
 package exercise1;
 
 public class Singers {
 
+    // Instance variables.
+    private int singerID;
+    private String singerName;
+    private String singerAddress;
+    private String singerDateOfBirth;
+    private int albumsPublished;
+
+    // Constructor with no arguments.
+    public Singers() {
+        this.singerID = 0;
+        this.singerName = null;
+        this.singerAddress = null;
+        this.singerDateOfBirth = null;
+        this.albumsPublished = 0;
+    }
+
+    // Constructor with all 5 arguments.
+    public Singers(int singerID, String singerName, String singerAddress, String singerDateOfBirth, int albumsPublished) {
+        this.singerID = singerID;
+        this.singerName = singerName;
+        this.singerAddress = singerAddress;
+        this.singerDateOfBirth = singerDateOfBirth;
+        this.albumsPublished = albumsPublished;
+    }
+
+    // Setters for instance variables of class Singers.
+    public void setSingerID(int singerID) { this.singerID = singerID; }
+    public void setSingerName(String singerName) { this.singerName = singerName; }
+    public void setSingerAddress(String singerAddress) { this.singerAddress = singerAddress; }
+    public void setSingerDateOfBirth(String singerDateOfBirth) { this.singerDateOfBirth = singerDateOfBirth; }
+    public void setAlbumsPublished(int albumsPublished) { this.albumsPublished = albumsPublished; }
+
+    // Setter to set all instance variables of class Singers at once.
+    public void setAllSingerValues(int singerID, String singerName, String singerAddress, String singerDateOfBirth, int albumsPublished) {
+        this.singerID = singerID;
+        this.singerName = singerName;
+        this.singerAddress = singerAddress;
+        this.singerDateOfBirth = singerDateOfBirth;
+        this.albumsPublished = albumsPublished;
+    }
+
+    // Getters for instance variables of class Singers.
+    public int getSingerID() { return singerID; }
+    public String getSingerName() { return singerName; }
+    public String getSingerAddress() { return singerAddress; }
+    public String getSingerDateOfBirth() { return singerDateOfBirth; }
+    public int getAlbumsPublished() { return albumsPublished; }
 }
--- Nicholas_COMP228Lab1/src/exercise1/SingersDriver.java
+++ Nicholas_COMP228Lab1/src/exercise1/SingersDriver.java
@@ -1,7 +1,48 @@
+// COMP228-406 - Lab 1 - Nicholas Bonneville
+// Lab 1 - Exercise 1
+
+// Write a Java application that creates a Java console application to keep records of singers and displays stored records.
+
 package exercise1;
 
 public class SingersDriver {
     public static void main(String[] args) {
 
+        // Create the singer1 object using the no argument constructor, then print it.
+        Singers singer1 = new Singers();
+        System.out.println("----------");
+        System.out.println("Default values for singer1:");
+        System.out.println("Singer ID: " + singer1.getSingerID());
+        System.out.println("Singer Name: " + singer1.getSingerName());
+        System.out.println("Singer Address: " + singer1.getSingerAddress());
+        System.out.println("Singer Date of Birth: " + singer1.getSingerDateOfBirth());
+        System.out.println("Number of Albums Published: " + singer1.getAlbumsPublished());
+        System.out.println("----------\n\n");
+
+        // Set all the values using the constructor.
+        singer1.setAllSingerValues(1, "AJR", "123 Main Street, Bayside, NY 11361", "January 1st, 2005", 5);
+        System.out.println("----------");
+        System.out.println("After setting all variables for singer1:");
+        System.out.println("Singer ID: " + singer1.getSingerID());
+        System.out.println("Singer Name: " + singer1.getSingerName());
+        System.out.println("Singer Address: " + singer1.getSingerAddress());
+        System.out.println("Singer Date of Birth: " + singer1.getSingerDateOfBirth());
+        System.out.println("Number of Albums Published: " + singer1.getAlbumsPublished());
+        System.out.println("----------\n\n");
+
+        // Set values using the setters individually.
+        singer1.setSingerID(2);
+        singer1.setSingerName("Tom Cochrane");
+        singer1.setSingerAddress("123 Secondary Avenue, Vancouver, BC V5C 6N5");
+        singer1.setSingerDateOfBirth("May 14th, 1953");
+        singer1.setAlbumsPublished(17);
+        System.out.println("----------");
+        System.out.println("After setting each value separately for singer1:");
+        System.out.println("Singer ID: " + singer1.getSingerID());
+        System.out.println("Singer Name: " + singer1.getSingerName());
+        System.out.println("Singer Address: " + singer1.getSingerAddress());
+        System.out.println("Singer Date of Birth: " + singer1.getSingerDateOfBirth());
+        System.out.println("Number of Albums Published: " + singer1.getAlbumsPublished());
+        System.out.println("----------\n\n");
     }
 }
