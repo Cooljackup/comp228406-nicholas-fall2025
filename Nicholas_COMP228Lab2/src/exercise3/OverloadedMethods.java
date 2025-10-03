03c2c8d94823610b33a9366bae895a5319068e6d
Cooljackup <cooljackup139@gmail.com>
2025-10-03 19:50:08 -0400

    Exercise 3 for lab 2 completed. Lab 2 completed.
    Finished both overloadedmethods and overloadedmethodsdriver. Exercise displays how a method can be overloaded using 3 different variable types when being added together, int, double, and string.

--- Nicholas_COMP228Lab2/src/exercise3/OverloadedMethods.java
+++ Nicholas_COMP228Lab2/src/exercise3/OverloadedMethods.java
@@ -7,4 +7,18 @@ package exercise3;
 
 public class OverloadedMethods {
 
+    // Overloading a method with 2 ints.
+    public static int overloadedSum(int a, int b) {
+        return a + b;
+    }
+
+    // Overloading a method with 2 doubles.
+    public static double overloadedSum(double a, double b) {
+        return a + b;
+    }
+
+    // Overloading a method with 2 strings.
+    public static String overloadedSum(String a, String b) {
+        return a + b;
+    }
 }
--- Nicholas_COMP228Lab2/src/exercise3/OverloadedMethodsDriver.java
+++ Nicholas_COMP228Lab2/src/exercise3/OverloadedMethodsDriver.java
@@ -5,8 +5,18 @@
 
 package exercise3;
 
+// Import necessary packages.
+import javax.swing.*;
+
 public class OverloadedMethodsDriver {
     public static void main(String[] args) {
 
+        // Instance variables.
+        int intResult = OverloadedMethods.overloadedSum(20, 30);
+        double doubleResult = OverloadedMethods.overloadedSum(15.5, 9.5);
+        String stringResult = OverloadedMethods.overloadedSum("Nicholas", "Bonneville");
+
+        // Displays all of the results.
+        JOptionPane.showMessageDialog(null, "Overloaded Methods:" + "\n\n2 ints added: 20 + 30 = " + intResult + "\n2 doubles added: 15.5 + 9.5 = " + doubleResult + "\n2 Strings added: Nicholas + Bonneville = " + stringResult);
     }
 }
