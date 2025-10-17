fbeab87ef2697a2af5934138a500e12de8a6eb26
Cooljackup <cooljackup139@gmail.com>
2025-10-17 02:49:06 -0400

    Exercise 3 started for lab 3.
    Started to prep exercise 3 for tomorrow. Only includes the comments and files, with just the instanced variables for both the constants and mortgage java files. Will expand on it tomorrow.

--- Nicholas_COMP228Lab3/src/exercise3/MortgageConstants.java
+++ Nicholas_COMP228Lab3/src/exercise3/MortgageConstants.java
@@ -1,4 +1,17 @@
+// COMP228-406 - Lab 3 - Nicholas Bonneville
+// Lab 3 - Exercise 3
+
+// Write a Java application that keeps track of mortgages and computes the total amount owed at any time (mortgage amount + interest).
+
 package exercise3;
 
 public class MortgageConstants {
+
+    // Instance variables.
+    int shortTerm = 1;
+    int mediumTerm = 3;
+    int longTerm = 5;
+    String bankName = "CityToronto Bank";
+    double maximumMortgageAmount = 300000;
+
 }
--- Nicholas_COMP228Lab3/src/exercise3/ProcessMortgage.java
+++ Nicholas_COMP228Lab3/src/exercise3/ProcessMortgage.java
@@ -1,4 +1,10 @@
+// COMP228-406 - Lab 3 - Nicholas Bonneville
+// Lab 3 - Exercise 3
+
+// Write a Java application that keeps track of mortgages and computes the total amount owed at any time (mortgage amount + interest).
+
 package exercise3;
 
 public class ProcessMortgage {
+
 }
