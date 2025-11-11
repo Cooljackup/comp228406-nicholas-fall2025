150130c75e4f4cb4d21ed34d2f8529c9c5a743cd
Cooljackup <cooljackup139@gmail.com>
2025-11-11 14:47:10 -0500

    Exercise created for Lab 4. main.java template also deleted.

---
+++ Nicholas_COMP228Lab4/src/exercise1/InformationApplication.java
@@ -0,0 +1,10 @@
+// COMP228-406 - Lab 4 - Nicholas Bonneville
+// Lab 4 - Exercise 1
+
+// Write a Java application using JavaFx which allows the user to enter student information.
+
+package exercise1;
+
+public class InformationApplication {
+
+}
